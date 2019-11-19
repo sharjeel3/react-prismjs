1b78499850e3bf1b6690da98bcc0680127ba67dd
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-19 16:50:54 +1100

    docs(cz): Add comment for cz-customizable config

--- cz.config.js
+++ cz.config.js
@@ -1,5 +1,6 @@
 const conventionalCommitTypes = require('conventional-commit-types').types;
 
+// cz-customizable config requires types as an array of objects with name and value
 const types = Object.keys(conventionalCommitTypes).map(type => ({
   name: `${type}: ${conventionalCommitTypes[type].description}`,
   value: type
