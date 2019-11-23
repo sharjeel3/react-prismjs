52f09663df66c435e8cfbe1446b11752d8290596
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-23 23:20:21 +1100

    build(rollup): Add terser plugin config

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
 import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
 
 const commonPlugins = [
   resolve(),
@@ -14,19 +15,26 @@ const commonPlugins = [
   commonjs()
 ];
 
+const getUmdConfig = (isProduction = false) => ({
+  input: 'src/index.js',
+  output: {
+    name: 'ReactPrismJS',
+    file: isProduction ? pkg.browser.replace('.js', '.min.js') : pkg.browser,
+    format: 'umd',
+    globals: {
+      react: 'React'
+    }
+  },
+  external: ['react'],
+  plugins: [...commonPlugins, isProduction && terser({})]
+});
+
 export default [
   {
-    input: 'src/index.js',
-    output: {
-      name: 'ReactPrismJS',
-      file: pkg.browser,
-      format: 'umd',
-      globals: {
-        react: 'React'
-      }
-    },
-    external: ['react'],
-    plugins: [...commonPlugins]
+    ...getUmdConfig()
+  },
+  {
+    ...getUmdConfig(true)
   },
   {
     input: 'src/index.js',
