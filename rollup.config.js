c9298d41e8cc01b96150240c240b49a49fd1bef9
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-23 20:37:11 +1100

    build(rollup): Update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import pkg from './package.json';
+import postcss from 'rollup-plugin-postcss';
 
 export default [
   {
@@ -9,27 +10,38 @@ export default [
     output: {
       name: 'ReactPrismJS',
       file: pkg.browser,
-      format: 'umd'
+      format: 'umd',
+      globals: {
+        react: 'React',
+        'react-dom': 'ReactDOM',
+        prismjs: 'Prism',
+        'prop-types': 'PropTypes'
+      }
     },
+    external: ['react'],
     plugins: [
       resolve(),
-      commonjs(),
+      postcss({}),
       babel({
-        exclude: ['node_modules/**']
-      })
+        exclude: ['node_modules/**'],
+        presets: ['@babel/preset-env', '@babel/preset-react']
+      }),
+      commonjs()
     ]
   },
   {
     input: 'src/index.js',
-    external: ['ms'],
     output: [
       { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
     ],
+    external: ['react'],
     plugins: [
       babel({
-        exclude: ['node_modules/**']
-      })
+        exclude: ['node_modules/**'],
+        presets: ['@babel/preset-env', '@babel/preset-react']
+      }),
+      postcss({})
     ]
   }
 ];
