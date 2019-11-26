7c81ce25f64a8b06c8bddfc5c517dd3c6cfe87fc
Sharjeel <ahmadsharjeel3@gmail.com>
2019-11-26 22:47:37 +1100

    refactor(index): Change component name

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import Prism from 'prismjs';
 import PropTypes from 'prop-types';
 import { Renderer } from './components/Renderer';
 
-const ReactPrismJS = ({ children, language = 'javascript', theme = '' }) => {
+const ReactPrism = ({ children, language = 'javascript', theme = '' }) => {
   const tokens = Prism.tokenize(children, Prism.languages[language]);
   return <Renderer tokens={tokens} theme={theme} language={language} />;
 };
 
-ReactPrismJS.propTypes = {
+ReactPrism.propTypes = {
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
   language: PropTypes.oneOf(['javascript', 'css', 'html', 'svg', 'xml', 'markup']).isRequired,
   theme: PropTypes.oneOf([
@@ -22,4 +22,4 @@ ReactPrismJS.propTypes = {
   ])
 };
 
-export default ReactPrismJS;
+export default ReactPrism;
--- src/index.test.js
+++ src/index.test.js
@@ -1,12 +1,12 @@
 /* eslint jest/expect-expect: 0 */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ReactPrismJS from './index';
+import ReactPrism from './index';
 
 describe('React Prism JS', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<ReactPrismJS language="javascript">const hello="world";</ReactPrismJS>, div);
+    ReactDOM.render(<ReactPrism language="javascript">const hello="world";</ReactPrism>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 });
